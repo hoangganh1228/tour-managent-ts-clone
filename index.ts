9ef95713dd1f910ceaffeff1f0096625419b3aac
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2024-09-09 21:25:05 +0700

    1.3. Cài đặt PUG

--- index.ts
+++ index.ts
@@ -3,8 +3,11 @@ import express, { Express, Request, Response } from "express";
 const app: Express = express();
 const port: number = 3000;
 
+app.set("views", "./views");
+app.set("view engine", "pug");
+
 app.get("/", (req: Request, res: Response) => {
-  res.send("Trang chủ");
+  res.render("client/pages/tours/index");
 });
 
 app.listen(port, () => {
