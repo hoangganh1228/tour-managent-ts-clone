21088311d61baf8171a730d49504036c01516c3f
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2024-09-10 14:59:45 +0700

    2.2. Tạo controllers

---
+++ controllers/client/tour.controller.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from "express";
+import Tour from "../../models/tour.model";
+
+export const index = async (req: Request, res: Response) => {
+  const tours = await Tour.findAll({
+    where: {
+      deleted: false,
+      status: "active"
+    },
+    raw: true
+  })
+
+
+  res.render("client/pages/tours/index", {
+    pageTitle: "Danh sách tour",
+    tours: tours
+  });
+}
--- routes/client/tour.route.ts
+++ routes/client/tour.route.ts
@@ -1,21 +1,8 @@
-import { Router, Request, Response } from "express";
-import Tour from "../../models/tour.model";
+import { Router } from "express";
+import * as controller from "../../controllers/client/tour.controller";
+
 const router: Router = Router();
 
-router.get("/", async (req: Request, res: Response) => {
-  const tours = await Tour.findAll({
-    where: {
-      deleted: false,
-      status: "active"
-    },
-    raw: true
-  })
-
-
-  res.render("client/pages/tours/index", {
-    pageTitle: "Danh sách tour",
-    tours: tours
-  });
-});
+router.get("/", controller.index);
 
 export const tourRoutes: Router = router;
