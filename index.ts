d041a9ea39c1ca6d9618cedfb925af34c5c3b2c6
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2024-09-10 14:53:27 +0700

    2.1. Tạo routes

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
-import express, { Express, Request, Response } from "express";
-
+import express, { Express } from "express"
 import dotenv from "dotenv";
 import sequelize from "./config/database";
-import Tour from "./models/tour.model";
+
+import clientRoutes from "./routes/client/index.route";
 
 
 
@@ -16,21 +16,8 @@ app.set("view engine", "pug");
 
 const port: number | string = process.env.PORT || 3000;
 
-app.get("/tours", async (req: Request, res: Response) => {
-  const tours = await Tour.findAll({
-    where: {
-      deleted: false,
-      status: "active"
-    },
-    raw: true
-  })
-
-
-  res.render("client/pages/tours/index", {
-    pageTitle: "Danh sách tour",
-    tours: tours
-  });
-});
+// Routes Client
+clientRoutes(app);
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
